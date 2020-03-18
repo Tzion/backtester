83ed6ecf57c2cd08624bfa3028ef53b70fa3a4d9
Tzion <tzion@intuitionrobotics.com>
2020-03-19 01:43:26 +0200

    first steps in the manual

--- samples/quickstart.py
+++ samples/quickstart.py
@@ -1,22 +1,41 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import backtrader as bt
 import datetime  # For datetime objects
 import os.path  # To manage paths
 import sys  # To find out the script name (in argv[0])
-from yahoo_finance import Share
 
-# Import the backtrader platform
-import backtrader as bt
+# Create a Stratey
+class TestStrategy(bt.Strategy):
+
+    def log(self, txt, dt=None):
+        ''' Logging function for this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+        self.open = self.datas[0].open
+        self.high= self.datas[0].high
+        self.low= self.datas[0].low
+
+
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f' % self.low[0])
 
 if __name__ == '__main__':
-    # Create a cerebro entity
     cerebro = bt.Cerebro()
+
+    # Add a strategy
+    cerebro.addstrategy(TestStrategy)
 
     # Datas are in a subfolder of the samples. Need to find where the script is
     # because it could have been called from anywhere
     modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
-    datapath = os.path.join(modpath, '../../datas/orcl-1995-2014.txt')
+    datapath = os.path.join(modpath, '../data_feeds/CSV.csv')
 
     # Create a Data Feed
     data = bt.feeds.YahooFinanceCSVData(
@@ -24,7 +43,7 @@ if __name__ == '__main__':
         # Do not pass values before this date
         fromdate=datetime.datetime(2000, 1, 1),
         # Do not pass values after this date
-        todate=datetime.datetime(2000, 12, 31),
+        todate=datetime.datetime(2018, 3, 10),
         reverse=False)
 
     # Add the Data Feed to Cerebro
@@ -33,11 +52,8 @@ if __name__ == '__main__':
     # Set our desired cash start
     cerebro.broker.setcash(100000.0)
 
-    # Print out the starting conditions
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
-    # Run over everything
     cerebro.run()
 
-    # Print out the final result
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
