d791f541b21911d2a9002524e87d7fb741b36a39
Tzion <tzion12345@gmail.com>
2021-05-11 18:41:34 +0300

    indicator contains strength and predicatablity for 7days only

--- iknowfirst/IkfIndicator.py
+++ iknowfirst/IkfIndicator.py
@@ -3,14 +3,16 @@ import pandas as pd
 
 class IkfIndicator(bt.Indicator):
    
-    lines = ('strength',)
-    params = (('value', 7),)
+    lines = ('strength', 'predictability')
+    params = (('timeframe', '7days'),)
 
     def __init__(self, forecast):
-        self.iter = iter(forecast)
+        self.forecast = forecast
+        self.iter_strength = iter(forecast.loc[:,self.p.timeframe,:]['strength'])
+        self.iter_pred = iter(forecast.loc[:,self.p.timeframe,:]['predictability'])
     
     def next(self):
-        self.lines.strength[0] = next(self.iter)
-        # self.lines.strength[0] = next(self.iter)
+        self.lines.strength[0] = next(self.iter_strength)
+        self.lines.predictability[0] = next(self.iter_pred)*100
 
 
