9f0471cadcce221fa957fd3b1d68fff7be1c5a7c
Tzion <tzion12345@gmail.com>
2021-06-22 14:52:19 +0300

    read all the stocks from the forecasts table - including the negative

--- iknowfirst/iknowfirst.py
+++ iknowfirst/iknowfirst.py
@@ -11,18 +11,24 @@ CACHE_FILE = 'cache.pkl'
 
 def extract_data_from_file(file_path):
     def extract_from_sheet(sheet_name, keys):
-        dataframe = pd.read_excel(file_path, sheet_name=sheet_name)
-        dataframes = []
-        for i in range(1, 19,6):
-            df_top = dataframe.iloc[4:7, i:i+5]
-            df_top = pd.DataFrame(data=df_top.iloc[1:].values.T, index=df_top.iloc[:1].values[0], columns=[
+        file_dataframe = pd.read_excel(file_path, sheet_name=sheet_name)
+        table_dataframes = []
+        for table_range in range(1, 19,6):
+            table_dataframe = pd.DataFrame()
+            for row_range in range(4,24,3):
+                row = file_dataframe.iloc[row_range:row_range+3, table_range:table_range+5]
+                row_df = pd.DataFrame(data=row.iloc[1:].values.T, index=row.iloc[:1].values[0], columns=[
                                   'strength', 'predictability'])
-            dataframes.append(df_top)
-        return pd.concat(dataframes, keys=keys)
+                row_df = row_df[~ row_df.index.isna()]  # filter out row with nan on the index
+                table_dataframe = pd.concat([table_dataframe, row_df])
+                # table_dataframe.sort_values(by=['strength', 'predictability'], ascending=False, inplace=True) # no need since the tables come sorted
+            table_dataframes.append(table_dataframe)
+        return pd.concat(table_dataframes, keys=keys)
 
     dataframes = extract_from_sheet('3-7-14days', ['3days', '7days', '14days']), extract_from_sheet('1-3-12months',['1months', '3months', '12months'])
     return pd.concat(dataframes)
 
+# design bug - scenario: 2 calls- use_cache=True then False - in the second call the dataframe returned is the one theat was load from the cache on the first call
 def retrieve_forecasts_data(forecasts_folder=FORECASTS_FOLDER, use_cache=True, filter_friday=True):
     if use_cache :
         load_from_cache()
@@ -37,7 +43,7 @@ def retrieve_forecasts_data(forecasts_folder=FORECASTS_FOLDER, use_cache=True, f
             dataframes.append(extract_data_from_file(f))
             dates.append(datetime.strptime(f[f.find('TA35_')+5:-4],'%d_%b_%Y'))
         dataframe = pd.concat(dataframes, keys=dates)
-        forecasts = dataframe.sort_index(axis='index', level=0, sort_remaining=True)
+        forecasts = dataframe.sort_index(axis='index', level=0, sort_remaining=False)
         dataframe.index.set_names(['date', 'timeframe', 'stock'], inplace=True)
     save_to_cache(forecasts)
     return forecasts.loc[filter(lambda i: i[0].dayofweek != 4, forecasts.index)] if filter_friday else forecasts
@@ -64,3 +70,11 @@ def retrieve_stocks():
     if forecasts is None:
         retrieve_forecasts_data()
     return list({i[2] for i in forecasts.index})
+
+
+def get_forecast_of(date, timeframe=None):
+    global forecasts
+    if timeframe: 
+        return forecasts.loc[str(date), timeframe] 
+    else:
+        return forecasts.loc[str(date)]
