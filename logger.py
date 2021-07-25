4c5cf10661db35c69db97f378efe05a61151c6ee
Tzion <tzion12345@gmail.com>
2021-07-25 12:14:26 +0300

    logger - poc using LoggerAdapter

--- logger.py
+++ logger.py
@@ -1,20 +1,38 @@
 import logging
 import sys
 
-ROOT_LOG_LEVEL = logging.INFO
+class FeedAdapter(logging.LoggerAdapter):
+    def process(self, msg, kwargs):
+        if self.extra['feedtime'] == '':
+            return "manipulating the message", kwargs
+        return msg,kwargs
 
 logging.LoggerAdapter
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter(fmt='%(asctime)s %(process)s-%(thread)s %(name)s %(levelname)s: %(message)s', datefmt='%y-%m-%d_%H:%M:%S'))
+handler.setFormatter(logging.Formatter(fmt='%(asctime)s %(process)s-%(thread)s %(name)s %(levelname)s: [%(feed)s@%(feedtime)s] %(message)s', datefmt='%y-%m-%d_%H:%M:%S'))
 handler.setStream(sys.stdout)
 logger.addHandler(handler)
+extra = {'feed':'', 'feedtime':''}
+logger = FeedAdapter(logger, extra)
 
-def logdebug(message):
-    logger.info("bueas")
+
+def logdebug(message, feed=None):
+    logger.debug(message, extra=extract_feed(feed))
+
+def extract_feed(feed):
+    extra = {'feed':'', 'feedtime':''}
+    if feed:
+        try: 
+            extra['feed']= feed._name
+            extra['feedtime']= feed.datetime.date()
+        except Exception as e:
+           extra['feed'] = 'ERROR' 
+    return extra
 
 
 def logdebugdeco(message):
     logdebug(message)
+
