411e4d3c753a6a6f34ebcc7d3aa7e2e2461ff1a1
Tzion <tzion12345@gmail.com>
2021-07-25 20:06:47 +0300

    config file, wip

--- logger.py
+++ logger.py
@@ -1,21 +1,26 @@
 import logging
+import logging.config
 import sys
 
 
-root_logger = logging.getLogger()
-root_logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter(fmt='%(asctime)s %(process)s-%(thread)s %(name)s %(levelname)s:  %(message)s', datefmt='%y-%m-%d_%H:%M:%S')
-file_handler = logging.FileHandler('./log1.log', 'w')
-file_handler.setFormatter(formatter)
-root_logger.addHandler(file_handler)
+# root_logger = logging.getLogger()
+# root_logger.setLevel(logging.DEBUG)
+# formatter = logging.Formatter(fmt='%(asctime)s %(process)s-%(thread)s %(name)s %(levelname)s:  %(message)s', datefmt='%y-%m-%d_%H:%M:%S')
+# file_handler = logging.FileHandler('./log1.log', 'w')
+# file_handler.setFormatter(formatter)
+# root_logger.addHandler(file_handler)
 
+# logger = logging.getLogger('backtester')
+# logger.setLevel(logging.DEBUG)
+# handler = logging.StreamHandler()
+# handler.setFormatter(formatter)
+# handler.setStream(sys.stdout)
+# logger.addHandler(handler)
+
+logging.config.fileConfig(fname='logger.conf', disable_existing_loggers=False)
+
+# Get the logger specified in the file
 logger = logging.getLogger('backtester')
-logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-handler.setStream(sys.stdout)
-logger.addHandler(handler)
-
 
 
 def logdebug(message, feed=None):
