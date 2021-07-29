bbce96d346ef9fc950f80810de46ee4e84c23f77
Tzion <tzion12345@gmail.com>
2021-07-29 14:45:23 +0300

    volume breakout

--- strategies/classic_breakout.py
+++ strategies/classic_breakout.py
@@ -25,10 +25,14 @@ class ClassicBreakout(TradeStateStrategy):
         feed.highest = indicators.Highest(feed.high, period=self.p.highs_period, subplot=False)
         feed.highest_breakout = feed.high > feed.highest(-1)
         feed.highest._name = 'somename' ## Workaround for bug in Bokeh - cannot print feed.highest(-1) without this attribute
-        feed.highest_breakout_marker = visualizers.SingleMarker(signals=feed.highest_breakout, level=feed.high)
+        feed.highest_breakout_marker = visualizers.SingleMarker(signals=feed.highest_breakout, level=feed.high ,plotmaster=feed)
         feed.volume_avg = indicators.SMA(feed.volume, period=self.p.highs_period, subplot=True) # feed.buy_level = visualizers.Partia, subplot=TrueFalsTrue, plotmaster=feeds_breakout, level=feed.low-2*feed.atr, plotmaster=feed,length=self.p.entry_period)
         feed.volume_peek = feed.volume > feed.volume_avg * 2
-        feed.volume_peek_marker = visualizers.SingleMarker(signals=feed.volume_peek, level=feed.low, color='blueviolet', marker='o')
+        feed.volume_peek_marker = visualizers.SingleMarker(signals=feed.volume_peek, level=feed.low, color='blueviolet', marker='o', plotmaster=feed)
+
+        # feed.buy_signal = bt.And(feed.highest_breakout, feed.volume_peek, plot=False) ## Bokeh cannot print this line
+        # feed.buy_signal_marker = visualizers.SingleMarker(signals=feed.buy_signal, level=feed.low*0.99, color='gold', marker='*')
+
 
     def initial_state_cls(self):
         return self.NoTrade
@@ -36,4 +40,6 @@ class ClassicBreakout(TradeStateStrategy):
 
     class NoTrade(TradeState):
         def next(self):
-            pass
+            # if self.feed.buy_signal[0]:
+            if self.feed.highest_breakout[0] and self.feed.volume_peek[0]:
+                self.strategy.buy_bracket(self.feed, exectype=bt.Order.Market, stopprice=self.feed.low[0], limitprice=self.feed.high[0]+self.feed.atr[0])
