74a11ed9b9eaeda2f87074c6ab8098bf90f772da
Tzion <tzion12345@gmail.com>
2021-08-12 13:24:05 +0300

    risk based sizer added

--- money_mgmt/sizers.py
+++ money_mgmt/sizers.py
@@ -24,3 +24,20 @@ class LongOnlyPortionSizer(PortionSizer):
         if not isbuy and not self.broker.getposition(data):
             return 0
         return super()._getsizing(comminfo, cash, data, isbuy)
+
+class RiskBasedSizer(bt.Sizer):
+
+    def __init__(self, risk_per_trade_percents=2.0):
+        self.risk_per_trade = risk_per_trade_percents/100
+
+    def _getsizing(self, comminfo, cash, data, isbuy):
+        risk = getattr(data, 'risk')
+        if not risk and not callable(risk):
+            raise Exception(f'{self.__class__.__name__} sizer requires {data} to have risk() method')
+
+        position = self.broker.getposition(data)
+        if position:
+            return position.size
+
+        size = (self.risk_per_trade * self.broker.getvalue()) / data.risk()
+        return size
