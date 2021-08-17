4d0f5f134c6cd1c3b1db778a25f50912b25b544c
Tzion <tzion12345@gmail.com>
2021-08-17 18:40:36 +0300

    exposer analyzer

--- analyzers/exposer.py
+++ analyzers/exposer.py
@@ -3,16 +3,24 @@ import backtrader as bt
 
 class Exposer(bt.Analyzer):
 
+    def __init__(self):
+        super().__init__()
+        if self.strategy.broker.params.shortcash:
+            raise Exception('This Analyzer works properly when broker.params.shortcash is False. use broker.set_shortcash(False)')
+
+
     def create_analysis(self):
         self.rets = OrderedDict()  # dict with . notation
-
         self.rets.len = 0
         self.rets.accumulative_exposer = 0.0
         self.rets.exposer = 0.0
 
     def next(self):
         self.rets.len += 1
-        current_exposer = self.strategy.broker.getcash() / self.strategy.broker.getvalue()
+        current_exposer = (self.strategy.broker.getvalue() - self.strategy.broker.getcash()) / self.strategy.broker.getvalue()
         self.rets.accumulative_exposer += current_exposer
         self.rets.exposer = self.rets.accumulative_exposer / self.rets.len
-        assert self.ret.len == len(self.strategy) == len(self)
+        assert self.rets.len == len(self.strategy) == len(self) # todo remove rets.len and use len(self)
+
+    def myprint(self):
+        print(f'Exposer: {self.rets.exposer*100}%')
