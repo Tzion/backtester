27234e42a2f4bc8086f0d7521f77dfcabad276eb
Tzion <tzion12345@gmail.com>
2021-08-17 19:12:09 +0300

    fix the exposer prints

--- analyzers/exposer.py
+++ analyzers/exposer.py
@@ -11,16 +11,11 @@ class Exposer(bt.Analyzer):
 
     def create_analysis(self):
         self.rets = OrderedDict()  # dict with . notation
-        self.rets.len = 0
         self.rets.accumulative_exposer = 0.0
         self.rets.exposer = 0.0
 
     def next(self):
-        self.rets.len += 1
         current_exposer = (self.strategy.broker.getvalue() - self.strategy.broker.getcash()) / self.strategy.broker.getvalue()
         self.rets.accumulative_exposer += current_exposer
-        self.rets.exposer = self.rets.accumulative_exposer / self.rets.len
-        assert self.rets.len == len(self.strategy) == len(self) # todo remove rets.len and use len(self)
-
-    def myprint(self):
-        print(f'Exposer: {self.rets.exposer*100}%')
+        self.rets.exposer = self.rets.accumulative_exposer / len(self.strategy)
+        self.rets['Exposer'] = str(self.rets.exposer * 100) + '%'
