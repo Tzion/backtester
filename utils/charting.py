01195e1fd410ed4c7efbc219b88132943c093276
Tzion <tzion12345@gmail.com>
2021-08-18 19:39:03 +0300

    use plotly to plot the pnl-bars simple chart (instead of bokeh)

--- utils/charting.py
+++ utils/charting.py
@@ -1,10 +1,9 @@
-from bokeh.plotting import show, figure
+import plotly.express as px
 import backtrader as bt
 
 
 def pnl_to_trade_length(trades: list[bt.Trade]):
     pnls = [t.pnl for t in trades]
     bars = [t.barlen for t in trades]
-    chart = figure(title="PnL to bars", x_axis_label='trade length (bars)', y_axis_label='profit and loss')
-    chart.scatter(x=bars, y=pnls)
-    show(chart)
+    fig = px.scatter(x=bars, y=pnls)
+    fig.show()
