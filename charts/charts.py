5d5d5de1ff34baf65e21320f1de2fc722a87763f
Tzion <tzion12345@gmail.com>
2021-08-21 18:20:23 +0300

    volume is added as subplot

--- charts/charts.py
+++ charts/charts.py
@@ -2,21 +2,31 @@ import plotly.graph_objects as go
 from datetime import datetime
 from plotly.subplots import make_subplots
 
-open_data = [33.0, 33.3, 33.5, 33.0, 34.1]
-high_data = [33.1, 33.3, 33.6, 33.2, 34.8]
-low_data = [32.7, 32.7, 32.8, 32.6, 32.8]
-close_data = [33.0, 32.9, 33.3, 33.1, 33.1]
-volume_data = [10, 2, 12, 42, 1, 43]
-dates = [datetime(year=2013, month=10, day=10),
-         datetime(year=2013, month=11, day=10),
-         datetime(year=2013, month=12, day=10),
-         datetime(year=2014, month=1, day=10),
-         datetime(year=2014, month=2, day=10)]
 
 def plot_feed(date, open, high, low, close, volume):
-    fig_data = [go.Candlestick(x=date, open=open, high=high, low=low, close=close)]
-    fig = go.Figure(data=fig_data)
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    prices_trace = go.Candlestick(x=date, open=open, high=high, low=low, close=close)
+    fig.add_trace(prices_trace, secondary_y=False)
+    volume_trace = go.Bar(x=date, y=volume)
+    fig.add_trace(volume_trace, secondary_y=True)
+
+    fig.update_layout(xaxis_type='category')  # workaround to handle gaps of dates when stock exchange is closed
+    fig.update_layout(xaxis_rangeslider_visible=False)
+
+    fig.show()
+
+def plot_feed2(date, open, high, low, close, volume):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True)
+
+    prices_trace = go.Candlestick(x=date, open=open, high=high, low=low, close=close)
+    fig.add_trace(prices_trace, row=1, col=1)
+    volume_trace = go.Bar(x=date, y=volume)
+    fig.add_trace(volume_trace, row=2, col=1)
+
+    # fig.update_layout(xaxis_type='category')  # workaround to handle gaps of dates when stock exchange is closed
+    fig.update_layout(xaxis_rangeslider_visible=False)
+
     fig.show()
 
 
-plot_feed(dates, open_data, high_data, low_data, close_data, volume_data)
--- tests/charts_test.py
+++ tests/charts_test.py
@@ -1,16 +1,36 @@
+from backtrader.utils.dateintern import num2date
 from test_common import *
 import backtrader as bt
-from datetime import datetime
+from datetime import date, datetime
 from charts.charts import plot_feed
 from utils.backtrader_helpers import extract_line_data as eld
 
 
-DATA = bt.feeds.GenericCSVData(dataname='tests/test_data.csv', fromdate=datetime(2016, 7, 1), todate=datetime(2017,6,30), dtformat='%Y-%m-%d', high=1, low=2, open=3, close=4, volume=5)
-cerebro = bt.Cerebro()
-cerebro.adddata(DATA)
-cerebro.addstrategy(DummyStrategy)
-strategy = cerebro.run()
+def csv_test():
+    DATA = bt.feeds.GenericCSVData(dataname='tests/test_data.csv', fromdate=datetime(2016, 7, 1), todate=datetime(2017,6,30), dtformat='%Y-%m-%d', high=1, low=2, open=3, close=4, volume=5)
+    cerebro = bt.Cerebro()
+    cerebro.adddata(DATA)
+    cerebro.addstrategy(DummyStrategy)
+    strategy = cerebro.run()
 
-data = strategy[0].data
-plot_feed(eld(data.datetime), eld(data.open), eld(data.high), eld(data.low), eld(data.close), eld(data.volume))
+    data = strategy[0].data
+    eld(data.datetime)
+    dates = list(map(lambda fdate : num2date(fdate).date(), eld(data.datetime)))
+    plot_feed(dates, eld(data.open), eld(data.high), eld(data.low), eld(data.close), eld(data.volume))
 
+def sample_test():
+    open_data = [33.0, 33.3, 33.5, 33.0, 34.1]
+    high_data = [33.1, 33.3, 33.6, 33.2, 34.8]
+    low_data = [32.7, 32.7, 32.8, 32.6, 32.8]
+    close_data = [33.0, 32.9, 33.3, 33.1, 33.1]
+    volume_data = [10, 2, 12, 42, 1, 43]
+    dates = [datetime(year=2013, month=10, day=10),
+            datetime(year=2013, month=11, day=10),
+            datetime(year=2013, month=12, day=10),
+            datetime(year=2014, month=1, day=10),
+            datetime(year=2014, month=2, day=10)]
+
+    plot_feed(dates, open_data, high_data, low_data, close_data, volume_data)
+
+if __name__ == '__main__':
+    csv_test()
