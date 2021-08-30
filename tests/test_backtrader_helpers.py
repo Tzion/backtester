baa22cf27c96e403f3a6ea67f7ef74c13e493bf5
Tzion <tzion12345@gmail.com>
2021-08-30 14:11:09 +0300

    2 test cases for extract_buynsell_observer

--- tests/test_backtrader_helpers.py
+++ tests/test_backtrader_helpers.py
@@ -1,7 +1,8 @@
-from .test_common import *
+from tests.test_common import *
 from backtrader import cerebro
 from utils import backtrader_helpers as bh
 import unittest
+import pytest
 
 
 class TestBacktraderHelpers(unittest.TestCase):
@@ -19,3 +20,14 @@ class TestBacktraderHelpers(unittest.TestCase):
 
     def test_extract_buynsell_observer(self):
         self.assertIsNotNone(bh.extract_buynsell_observer(strategy))
+
+
+def test_extract_buynsell_observer__non_default_name():
+    cerebro = bt.Cerebro(stdstats=False)
+    cerebro.adddata(DUMMY_DATA)
+    cerebro.addobserver(bt.observers.BuySell, obsname='non_default_name')
+    cerebro.addstrategy(DummyStrategy)
+    strategy = cerebro.run()[0]
+    assert bh.extract_buynsell_observer(strategy) is not None
+
+
