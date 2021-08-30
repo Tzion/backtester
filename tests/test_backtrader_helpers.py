4585584a9f129646f9c5d17f5a17ae86fa6ccf58
Tzion <tzion12345@gmail.com>
2021-08-30 14:19:02 +0300

    migrating to pytest

--- tests/test_backtrader_helpers.py
+++ tests/test_backtrader_helpers.py
@@ -5,21 +5,17 @@ import unittest
 import pytest
 
 
-class TestBacktraderHelpers(unittest.TestCase):
+@pytest.fixture
+def strategy():
+    cerebro = bt.Cerebro()
+    cerebro.adddata(DUMMY_DATA)
+    cerebro.addstrategy(DummyStrategy)
+    strategy = cerebro.run()[0]
+    yield strategy
 
-    @classmethod
-    def setUpClass(cls) -> None:
-        global cerebro, strategy
-        cerebro = bt.Cerebro()
-        cerebro.adddata(DUMMY_DATA)
-        cerebro.addobserver(bt.observers.BuySell, obsname='non_default_name')
-        cerebro.addstrategy(DummyStrategy)
-        strategy = cerebro.run()[0]
-
-
-
-    def test_extract_buynsell_observer(self):
-        self.assertIsNotNone(bh.extract_buynsell_observer(strategy))
+class TestBacktraderHelpers():
+    def test_extract_buynsell_observer(self, strategy):
+        assert bh.extract_buynsell_observer(strategy) is not None
 
 
 def test_extract_buynsell_observer__non_default_name():
