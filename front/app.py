3ad1067d6f1f710624fece5e6bd733ea8ceed106
Tzion <tzion12345@gmail.com>
2021-10-10 13:23:28 +0300

    live updates of trade list (1 sec interval)

--- front/app.py
+++ front/app.py
@@ -1,17 +1,23 @@
 import dash
-# import dash_html_components as html
+from dash import dcc
 from dash import html
 import os
+
+from dash.dependencies import Input, Output
 from globals import CHARTS_DIR
 
-app = dash.Dash(__name__, assets_folder='../'+CHARTS_DIR)
+app = dash.Dash(__name__, assets_folder='../' + CHARTS_DIR)
 
 
-chart_links = [html.A(children=[file_name, html.Br()], href='assets/'+file_name) for file_name in os.listdir(CHARTS_DIR)]
 app.layout = html.Div(children=[
     html.H1(children='Charts'),
-    html.Div(children=chart_links),
+    html.Div(id='trade-list', children=[]),
+    dcc.Interval(id='trade-list-interval', interval=1000, n_intervals=0)
 ])
+
+@app.callback(Output('trade-list', 'children'), Input('trade-list-interval', 'n_intervals'))
+def update_trade_list(n):
+    return [html.A(children=[file_name, html.Br()], href='assets/'+file_name) for file_name in os.listdir(CHARTS_DIR)]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
