c12e3c1c9c6a02fb66d7fc216e42894d83143c04
Tzion <tzion12345@gmail.com>
2021-10-17 17:06:37 +0300

    modify the trailing stop test

--- tests/orders_manual_test.py
+++ tests/orders_manual_test.py
@@ -9,10 +9,10 @@ class StopTrailTest(bt.Strategy):
     def next(self):
         if not self.getposition():
             entry = self.buy(price=11, exectype=bt.Order.Limit)
-            self.sell(exectype=bt.Order.StopTrail, trailamount=.5)
+            self.sell(exectype=bt.Order.StopTrail,price=10, trailamount=2)
 
 if __name__ == '__main__':
-    prices = np.tile([10,11,12,11.5,11.2,15,14,13,14,15],(7,1)).transpose()
+    prices = np.tile([10,11,10,10.2,9.2,9.0,9.1,10,13,14],(7,1)).transpose()
     DATA = PandasData(dataname=pd.DataFrame(data=prices, columns=PandasData.datafields, index=pd.date_range('20210818', periods=10)))
     cerebro = bt.Cerebro()
     cerebro.adddata(DATA)
