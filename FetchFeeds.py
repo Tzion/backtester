352eea98da3bd81597408d3477f8a8d08d419c59
Tzion <tzion12345@gmail.com>
2021-10-19 11:13:11 +0300

    fix the feed fetcher - make it work again

--- FetchFeeds.py
+++ FetchFeeds.py
@@ -1,19 +1,21 @@
 import pandas as pd
-import pandas_datareader as pdr
 import os
+import yfinance as yf
 
 data = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
 table = data[0]
 tickers = table['Symbol'].tolist()
+FEEDS_DIR = './data_feeds/'
 
 # todo validate data_feed directory
 lost = []
 for symbol in tickers:
     try:
-        if not os.path.exists('./data_feeds/{}.csv'.format(symbol)):
+        if not os.path.exists(f'{FEEDS_DIR}{symbol}.csv'):
             print('downloading data feed of {}'.format(symbol))
-            feed = pdr.get_data_yahoo(symbol.replace('.',''))
-            feed.to_csv('./data_feeds/{}.csv'.format(symbol))
+            symbol = symbol.replace('.','')
+            feed = yf.download(symbol, start=None, end=None, progress=True) #  when start and end are None gives all histroy until today.
+            feed.to_csv(f'{FEEDS_DIR}{symbol}.csv')
         else:
             print('data feed of {} already exists, skipping'.format(symbol))
     except Exception as e:
