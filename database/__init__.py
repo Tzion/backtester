cf4553950cdda52dc3e19084b6069cd3b4ba4070
Tzion <tzion12345@gmail.com>
2021-11-22 15:00:44 +0200

    database environement - use enum

--- database/__init__.py
+++ database/__init__.py
@@ -1,22 +1,23 @@
 import sys
+import enum
 
 PROD_FLAG = '--prod'
 
-class DatabaseEnv:
+class DatabaseEnv(enum.Enum):
     TESTING, PRODUCTION = range(1,3)
 
     @classmethod
     def get(cls):
-        return ENV
+        return DATABASE_ENV.value
 
     @classmethod
     def get_name(cls):
-        return list(DatabaseEnv.__dict__.keys())[ENV]
+        return DATABASE_ENV.name
 
-ENV = DatabaseEnv.TESTING
+DATABASE_ENV = DatabaseEnv.TESTING
 
 if PROD_FLAG in sys.argv:
-    ENV = DatabaseEnv.PRODUCTION
+    DATABASE_ENV = DatabaseEnv.PRODUCTION
     
 
 def get_stored_feed_file_path(symbol):
