acf47dce7b8edc31a0f3156e76d3009108b187bb
Tzion <tzion12345@gmail.com>
2021-11-29 13:07:35 +0200

    fid diffs in csv files - first phase

--- database/__init__.py
+++ database/__init__.py
@@ -42,3 +42,47 @@ def get_feed_file_path(symbol):
     
                                             
     """
+    
+# %%
+    
+file_1 = 'TESTING/trading_view/22lines.csv'
+file_2 = 'TESTING/trading_view/22lines_missing_date_middle.csv'
+
+def diff_data_feed_csv(file_1, file_2, columns=['open', 'low', 'high', 'close',]):
+    # TODO support date range
+    df1 = pd.read_csv(file_1, parse_dates=[0])
+    df2 = pd.read_csv(file_2, parse_dates=[0])
+    columns.insert(0,df1.columns[0])
+    df1 = pd.DataFrame(df1, columns=columns)
+    df2 = pd.DataFrame(df2, columns=columns)
+    equal = df1.eq(df2)
+    if equal.all().all():
+        return False  # no diffs between dataframes
+    else:
+        return equal
+    # to show details results - next phase
+    compare = df11[df11.eq(df22).all(axis=1) == False]
+    print(comparison.to_string(index=True))
+
+
+# %%
+import pandas as pd
+
+# %%
+columns = ['open', 'low']
+
+# %%
+df1 = pd.read_csv(file_1, parse_dates=[0])
+df2 = pd.read_csv(file_2, parse_dates=[0])
+#%%
+columns.insert(0,df1.columns[0])
+df11 = pd.DataFrame(df1, columns=columns)
+df22 = pd.DataFrame(df2, columns=columns)
+    
+# %%
+df11.eq(df22)
+# %%
+comparison = df11[df11.eq(df22).all(axis=1) == False]
+print(comparison.to_string(index=True))
+
+# %%
