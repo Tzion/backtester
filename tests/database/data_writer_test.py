c414840d8707af682afdd480313e2a92a09764af
Tzion <tzion12345@gmail.com>
2022-01-18 13:34:13 +0200

    unify 3 tests to one

--- tests/database/data_writer_test.py
+++ tests/database/data_writer_test.py
@@ -10,38 +10,26 @@ data_path = TEST_DATA_DIR+'/writer_test.csv'
 
 #TODO files in tmpdir are not cleaned!
 
-#TODO unfiy 3 tests to one as: write to new file, add new data point and rewrite. 
-# The added value is that we're validate that the write and append, keep on the same format
 @pytest.mark.parametrize('data', [([bt.feeds.GenericCSVData(dataname=data_path, dtformat='%Y-%m-%d', 
                                                             fromdate=datetime(2020, 11, 2), todate=datetime(2020,11,14))])])
-def test_store_to_new_file(data_fixture: bt.feed.FeedBase, tmpdir):
-    tmpfile = tmpdir.join("tmpfile.csv")
-    store(data_fixture, tmpfile)
-    diffs = diff_data_feed_csv(data_path, tmpfile)
-    assert diffs.empty, f'There are differences between the files:\n{diffs.to_string(index=True)}\n'
+class TestStore:
+    def test_store_and_add_new_datapoint(self,data_fixture: bt.feed.FeedBase, tmpdir):
+        tmpfile = tmpdir.join("tmpfile.csv")
+        store(data_fixture, tmpfile)
+        diffs = diff_data_feed_csv(data_path, tmpfile)
+        assert diffs.empty, f'There are differences between the files:\n{diffs.to_string(index=True)}\n'
+        # add new data point and rewrite
+        new_datapoint = extend_last_datapoint_by1(data_fixture)
+        store(data_fixture, tmpfile)
+        diffs = diff_data_feed_csv(data_path, tmpfile)
+        assert len(diffs) == 1, f'Expecting one additional datapoint'
+        assert contains_datapoint(new_datapoint, tmpfile), f'Datapoint is not found in the file'
 
 
-@pytest.mark.parametrize('data', [([bt.feeds.GenericCSVData(dataname=data_path, dtformat='%Y-%m-%d', 
-                                                            fromdate=datetime(2020, 11, 2), todate=datetime(2020,11,14))])])
-def test_store_to_existing_file(data_fixture: bt.feed.FeedBase, tmpdir):
-    tmpfile = tmpdir.join("tmpfile.csv")
-    copy(data_path, str(tmpfile))
-    store(data_fixture, tmpfile)
-    diffs = diff_data_feed_csv(data_path, tmpfile)
-    assert diffs.empty, f'There are differences between the files:\n{diffs.to_string(index=True)}\n'
-
-@pytest.mark.parametrize('data', [([bt.feeds.GenericCSVData(dataname=data_path, dtformat='%Y-%m-%d', 
-                                                            fromdate=datetime(2020, 11, 2), todate=datetime(2020,11,14))])])
-def test_store_new_datapoint(data_fixture: bt.feed.FeedBase, tmpdir):
+def test_store_merge_conflict(data_fixture: bt.feed.FeedBase, tmpdir):
     tmpfile = tmpdir.join("tmpfile.csv")
     copy(data_path, str(tmpfile))
-    new_datapoint = extend_last_datapoint_by1(data_fixture)
     store(data_fixture, tmpfile)
-    diffs = diff_data_feed_csv(data_path, tmpfile)
-    assert len(diffs) == 1, f'Expecting one additional datapoint'
-    assert contains_datapoint(new_datapoint, tmpfile), f'Datapoint is not found in the file'
-
-def test_store_merge_conflict():
     pass
 
 def test_store_weekend_datapoint():
