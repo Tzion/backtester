9b5950bd9cf9e00205f92be5bd0c49053e952db9
Tzion <tzion12345@gmail.com>
2022-01-18 21:35:56 +0200

    prepare fixtures for filepath that is safe to write to

--- tests/database/data_writer_test.py
+++ tests/database/data_writer_test.py
@@ -5,6 +5,7 @@ from shutil import copy
 from database.data_writer import store
 from database import diff_data_feed_csv
 from backtrader import num2date
+from database import FeedMergeException
     
 data_path = TEST_DATA_DIR+'/writer_test.csv'
 
@@ -13,7 +14,7 @@ data_path = TEST_DATA_DIR+'/writer_test.csv'
 @pytest.mark.parametrize('data', [([bt.feeds.GenericCSVData(dataname=data_path, dtformat='%Y-%m-%d', 
                                                             fromdate=datetime(2020, 11, 2), todate=datetime(2020,11,14))])])
 class TestStore:
-    def test_store_and_add_new_datapoint(self,data_fixture: bt.feed.FeedBase, tmpdir):
+    def test_store_and_add_new_datapoint(self, data_fixture: bt.feed.FeedBase, tmpdir):
         tmpfile = tmpdir.join("tmpfile.csv")
         store(data_fixture, tmpfile)
         diffs = diff_data_feed_csv(data_path, tmpfile)
@@ -26,22 +27,43 @@ class TestStore:
         assert contains_datapoint(new_datapoint, tmpfile), f'Datapoint is not found in the file'
 
 
-def test_store_merge_conflict(data_fixture: bt.feed.FeedBase, tmpdir):
-    tmpfile = tmpdir.join("tmpfile.csv")
-    copy(data_path, str(tmpfile))
-    store(data_fixture, tmpfile)
-    pass
+    def test_store_merge_conflict(self, data_fixture: bt.feed.FeedBase, tmpdir):
+        tmpfile = tmpdir.join("tmpfile.csv")
+        copy(data_path, str(tmpfile))
+        data_fixture.open[0] = 0
+        store(data_fixture, tmpfile)
+        diffs = diff_data_feed_csv(data_path, tmpfile)
+        assert diffs.empty, f'Manipulated data should not be written \n{diffs.to_string(index=True)}\n'
 
-def test_store_weekend_datapoint():
-    pass
+
+    def test_store_weekend_datapoint(self, write_safe_data_copy):
+        return
+        data_fixture, tmpfile = write_safe_data_copy
+
+
+@pytest.mark.parametrize('data', [([bt.feeds.GenericCSVData(dataname=data_path, dtformat='%Y-%m-%d', 
+                                                            fromdate=datetime(2020, 11, 2), todate=datetime(2020,11,14))])])
+@pytest.fixture
+def write_safe_data(data_fixture, tmpdir):
+    '''Provies initialized data and writable filepath'''
+    tmp_path = tmpdir.join("StoreTest_tempfile.csv")
+    return data_fixture, tmp_path
+
+@pytest.mark.parametrize('data', [([bt.feeds.GenericCSVData(dataname=data_path, dtformat='%Y-%m-%d', 
+                                                            fromdate=datetime(2020, 11, 2), todate=datetime(2020,11,14))])])
+@pytest.fixture
+def write_safe_data_copy(data_fixture, tmpdir):
+    '''Provies initialized data and writable filepath with copy of the data'''
+    tmp_path = tmpdir.join("StoreTest_tempfile_copy.csv")
+    copy(data_path, str(tmp_path))
+    return data_fixture, tmp_path
 
 def extend_last_datapoint_by1(data_fixture):
     data_fixture.forward()
-    data_point = {}
-    for line in data_fixture.lines:
-        line[0] = line[-1] + 1
+    data_point = {} 
     for alias in data_fixture.lines.getlinealiases():
         line = getattr(data_fixture.lines, alias)
+        line[0] = line[-1] + 1
         data_point[alias] = line[-1] + 1
     return data_point
 
