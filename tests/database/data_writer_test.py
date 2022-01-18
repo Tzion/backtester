3bbd5da055ca138a248e434911f94715f2796bfb
Tzion <tzion12345@gmail.com>
2022-01-18 22:39:11 +0200

    add test of failed try to store weekend days (test doens't pass - missing functionality)

--- tests/database/data_writer_test.py
+++ tests/database/data_writer_test.py
@@ -20,23 +20,32 @@ class TestStore:
         diffs = diff_data_feed_csv(data_path, tmpfile)
         assert diffs.empty, f'There are differences between the files:\n{diffs.to_string(index=True)}\n'
         # add new data point and rewrite
-        new_datapoint = extend_last_datapoint_by1(data_fixture)
+        new_datapoint = extend_last_datapoint_by_1(data_fixture)
         store(data_fixture, tmpfile)
         diffs = diff_data_feed_csv(data_path, tmpfile)
         assert len(diffs) == 1, f'Expecting one additional datapoint'
         assert contains_datapoint(new_datapoint, tmpfile), f'Datapoint is not found in the file'
 
-
-    def test_store_merge_conflict(self, data_fixture: bt.feed.FeedBase, tmpdir):
+    def test_fails_store_merge_conflict(self, data_fixture: bt.feed.FeedBase, tmpdir):
         tmpfile = tmpdir.join("tmpfile.csv")
         copy(data_path, str(tmpfile))
-        data_fixture.open[0] = 0
+        data_fixture.open[0] = 0  # create a conflict
         store(data_fixture, tmpfile)
         diffs = diff_data_feed_csv(data_path, tmpfile)
-        assert diffs.empty, f'Manipulated data should not be written \n{diffs.to_string(index=True)}\n'
+        assert diffs.empty, f'Manipulated data should not be written (hence no diffs with the untouched original file)\n{diffs.to_string(index=True)}\n'
 
 
-def extend_last_datapoint_by1(data_fixture):
+    def test_fails_store_weekend_datapoint(self, data_fixture: bt.feed.FeedBase, tmpdir):
+        tmpfile = tmpdir.join("tmpfile.csv")
+        copy(data_path, str(tmpfile))
+        initial_len = len(data_fixture)
+        extend_enough_datapoints_to_reach_weekend(data_fixture)
+        assert len(data_fixture) >= 5 + initial_len, 'Data was not extended enough'
+        store(data_fixture, tmpfile)
+        diffs = diff_data_feed_csv(tmpfile, data_path)
+        assert diffs.empty, f'Manipulated data should not be written (hence no diffs with the untouched original file)\n{diffs.to_string(index=True)}\n'
+
+def extend_last_datapoint_by_1(data_fixture):
     data_fixture.forward()
     data_point = {} 
     for alias in data_fixture.lines.getlinealiases():
@@ -45,6 +54,9 @@ def extend_last_datapoint_by1(data_fixture):
         data_point[alias] = line[-1] + 1
     return data_point
 
+def extend_enough_datapoints_to_reach_weekend(data_fixture):
+    [extend_last_datapoint_by_1(data_fixture) for _ in range(5)]
+    
 def contains_datapoint(datapoint, file):
     index_label = 'datetime'
     dataframe = pd.read_csv(file, index_col=index_label,)
