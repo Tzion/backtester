9f804ceb8f0df5590932350a85b290fcc248618a
Tzion <tzion12345@gmail.com>
2022-02-01 12:32:25 +0200

    fix bug in the symbol names of IB data source

--- database/data_source.py
+++ database/data_source.py
@@ -5,7 +5,7 @@ PREFIX = 'database/data/'
 class DataSource(ABC):
 
     @abstractmethod
-    def get_feed_filename(self, symbol: str) -> str:
+    def get_feed_fullname(self, symbol: str) -> str:
         pass
 
     @property
@@ -14,13 +14,13 @@ class DataSource(ABC):
         pass
 
     def get_feed_path(self, symbol: str) -> str:
-        return PREFIX + '/' + self.name.lower() +'/' + self.get_feed_filename(symbol)
+        return PREFIX + '/' + self.name.lower() +'/' + self.get_feed_fullname(symbol)
             
 
 class IBDataSource(DataSource):
 
-    def get_feed_filename(self, symbol: str):
-        return symbol + '-STK-SMART-USD.csv'
+    def get_feed_fullname(self, symbol: str):
+        return symbol + '-STK-SMART-USD'
 
     @property
     def name(self) -> str:
