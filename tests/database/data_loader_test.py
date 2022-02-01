7805e0496b1f3fa18b2932b48dac6380f8563ab7
Tzion <tzion12345@gmail.com>
2022-02-01 11:17:05 +0200

    Connectivity Warning were added to the data loader test

--- tests/database/data_loader_test.py
+++ tests/database/data_loader_test.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import pytest
 from __init__test import TEST_DATA_DIR
 from database import diff_data_feed, feed_to_dataframe
-
+import functools
 
 def assert_prices(feed, datetime, open, high, low, close, ago=0):
     assert feed.datetime.date(-ago) == datetime.date()
@@ -14,6 +14,17 @@ def assert_prices(feed, datetime, open, high, low, close, ago=0):
     assert feed.low[-ago] == low
     assert feed.close[-ago] == close
 
+
+def connectivity_warning(test):
+    @functools.wraps(test)
+    def test_wrapper(self, *args, **kwargs):
+        try:
+            test(self, *args, **kwargs)
+        except Exception as e:
+            raise Warning('Make sure IB Gateway or TWS is connected') from e
+    return test_wrapper
+
+
 class TestIBLoader:
     """TWS or IB Gateway MUST BE CONNECTED prior to these tests"""
         
@@ -21,8 +32,7 @@ class TestIBLoader:
     def loader(self, cerebro):
         return IBLoader(cerebro)
 
-    #TODO add exception message to check the connectivity
-    
+    @connectivity_warning
     @pytest.mark.parametrize('start, end', [('2020-11-02', '2021-03-19')])
     def test_request_feed_data_for_period(self, cerebro, loader, start, end, mocker):
         static_data = TEST_DATA_DIR + 'request_test-ZION-IB.csv'
@@ -36,6 +46,7 @@ class TestIBLoader:
         diffs = diff_data_feed(requested_df, df.loc[requested_df.index[0]:requested_df.index[-1]], columns=['open', 'low', 'high', 'close','volume'])
         assert diffs.empty, f'There are differences between the requested data and the static data:\n{diffs.to_string(index=True)}\n'
 
+    @connectivity_warning
     def test_request_feed_data_on_weekend(self, cerebro, loader):
         """Request data over the weekend when there is no trading of the contract"""
         weekend_start = datetime(2021,11,13)
@@ -43,12 +54,13 @@ class TestIBLoader:
         weekend_end = datetime(2021, 11,15)
         assert weekend_end.isoweekday() == 1
         loader.load_feeds(['ZION'], weekend_start, weekend_end, backfill_from_database=False, store=False)
-        loader.load_feeds(['NVDA'], datetime(2021,11,15), datetime(2021,11,22), backfill_from_database=False)
+        loader.load_feeds(['NVDA'], datetime(2021,11,15), datetime(2021,11,22), backfill_from_database=False, store=False)
         cerebro.addstrategy(test_common.DummyStrategy)
         cerebro.run()
         assert len(cerebro.datas[0]) == 0, 'data should be empty - since it\'s weekend'
         assert len(cerebro.datas[1]) == 5, 'data should have full business week - 5 days'
         
+    @connectivity_warning
     def test_backfill_one_bar(self, cerebro, loader, mocker):
         """Load data feed from file, fill missing bar from live data server"""
         mocker.patch.object(loader.source, 'get_feed_path', return_value=TEST_DATA_DIR + 'backfill_test.csv')
@@ -58,3 +70,4 @@ class TestIBLoader:
         assert len(cerebro.datas[0]) == 2
         assert_prices(cerebro.datas[0], datetime(2021, 11, 19), 44.44, 66.66, 33.33, 55.55, ago=1), 'Mismatch with data from file'
         assert_prices(cerebro.datas[0], datetime(2021, 11, 22), 335.17, 346.47, 319.0, 319.56, ago=0), 'Mismatch with data from server'
+
